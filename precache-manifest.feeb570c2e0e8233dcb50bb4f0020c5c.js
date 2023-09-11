c5a2d173256d6019ec40814925ea4679e239c4ec
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2023-09-11 17:52:26 +0530

    Updates

---
+++ precache-manifest.feeb570c2e0e8233dcb50bb4f0020c5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a869f8c086713e05a0",
+    "url": "/WordSearchGame/static/css/main.cdf1b911.chunk.css"
+  },
+  {
+    "revision": "19a869f8c086713e05a0",
+    "url": "/WordSearchGame/static/js/main.19a869f8.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/WordSearchGame/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "bb421e23a1d65c72ceb4",
+    "url": "/WordSearchGame/static/js/2.bb421e23.chunk.js"
+  },
+  {
+    "revision": "170cb6f356d1c03f2d46",
+    "url": "/WordSearchGame/static/js/runtime~main.170cb6f3.js"
+  },
+  {
+    "revision": "de7c0cf36488e8bae779a91a49d5d0cd",
+    "url": "/WordSearchGame/static/media/win.de7c0cf3.mp3"
+  },
+  {
+    "revision": "3d23644f6c012c3bee87493f6603f3ed",
+    "url": "/WordSearchGame/static/media/bg.3d23644f.png"
+  },
+  {
+    "revision": "c646bcc9044706aec98c80ee5d28fb14",
+    "url": "/WordSearchGame/index.html"
+  }
+];
